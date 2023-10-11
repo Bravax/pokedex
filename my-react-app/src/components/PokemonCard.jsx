e9943e965f17382624abe7381f9f76d15baa3fef
Xavier Lobez <xavierlobez@hotmail.fr>
2023-10-11 11:53:03 +0200

    create pokemonCard

--- my-react-app/src/components/PokemonCard.jsx
+++ my-react-app/src/components/PokemonCard.jsx
@@ -1,9 +1,23 @@
 function PokemonCard() {
-    return (
-    <figure>
-        <img src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
-        <figcaption>Bulbasaur</figcaption>
-    </figure>);
-}
+    const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+    ];   
+    const pokemon = pokemonList [0]
+   return (
+       <>
+       {pokemon.name}
+       {pokemon.imgSrc ? <img src = {pokemon.imgSrc} /> : <p>???</p>}
+       </>  
+      );
+    }
+    
 
+console.log();
 export default PokemonCard;
